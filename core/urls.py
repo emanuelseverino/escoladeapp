fcf4dabe162ba08d2e9f3ecc43fa1593e5c7fc6c
emanuelseverino <40120367+emanuelseverino@users.noreply.github.com>
2022-06-02 10:57:26 -0300

    Criando a página de cadastro

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,10 @@ from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
 
 from core.views import IndexView, ObrigadoView
+from usuario.views import RegisterView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('obrigado/', ObrigadoView.as_view(), name='obrigado'),
+    path('cadastro/', RegisterView.as_view()),
 ]
--- usuario/urls.py
+++ usuario/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from usuario.views import RegisterView
 
 urlpatterns = [
-    path('cadastro/', RegisterView.as_view()),
+
 ]
